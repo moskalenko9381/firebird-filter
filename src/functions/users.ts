75589bd9003dec7239acf33ca14d9cca7c6f9894
moskalenko9381 <lizamosk2001@mail.ru>
2023-08-08 00:04:17 +0300

    small filter fix

--- src/functions/users.ts
+++ src/functions/users.ts
@@ -2,7 +2,7 @@ export const getIndexes = (source: string, substring: string) => {
     const occurenceIndexes: number[] = [];
     const length = substring.length;
     let index = source.indexOf(substring);
-    while (index >= 0 && index + length < source.length) {
+    while (index >= 0 && index + length <= source.length) {
         occurenceIndexes.push(index);
         const sliced = source.slice(index + length);
         const newIndex = sliced.indexOf(substring);
@@ -11,5 +11,6 @@ export const getIndexes = (source: string, substring: string) => {
         }
         index = newIndex + Math.abs(source.length - sliced.length);
     }
+    console.log("Indexes:", occurenceIndexes, source, substring, source.length);
     return occurenceIndexes;
 };
